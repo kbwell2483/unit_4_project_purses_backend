be8c181fc4a3c507a9256f23969964d0eb8e06ed
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-10-25 03:16:20 -0500

    attempting to deploy to Heroku

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
-class WelcomeController < ApplicationController
-    def index
-        render json: { status: 200, message: "Purse API" }
-      end
-end
+# class WelcomeController < ApplicationController
+#     def index
+#         render json: { status: 200, message: "Purse API" }
+#       end
+# end
